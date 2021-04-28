d10663ef4c340da53328c3c473d763d6a8a1509c
Alex Kenty <ak5kenty@gmail.com>
2021-04-28 14:45:49 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/UI/Header';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-        Hello React!
+        <Header />
+        <Login/>
     </div>
   );
 }
